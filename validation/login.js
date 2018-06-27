ff77a1a1aba6c4b486cd3b82d15c7e367bcdb1e1
John Dawes <jdawes@bluebitmedia.com>
2018-06-27 13:05:02 -0600

    Login API Validation

--- validation/login.js
+++ validation/login.js
@@ -12,20 +12,8 @@ module.exports = validateLoginInput = data => {
   // Convert empty object properties to empty string, and
   // Gracefully error out in case form is not submitted as
   // x-www-form-urlencoded
-  data.name = !isEmpty(data.name) ? data.name : "";
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-
-  // Name Min and Max length
-  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = "Name must be between 2 and 30 characters";
-  }
-
-  // Name required
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Name is required";
-  }
 
   // Email regex
   if (!Validator.isEmail(data.email)) {
@@ -39,22 +27,12 @@ module.exports = validateLoginInput = data => {
 
   // Password Min and Max length
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
+    errors.password = "Invalid Password";
   }
 
   // Password required
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password is required";
-  }
-
-  // Password compare
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
-  }
-
-  // Password Confirm required
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm password is required";
   }
 
   return { errors, isValid: isEmpty(errors) }; // returns boolean based on errors object being empty or not
