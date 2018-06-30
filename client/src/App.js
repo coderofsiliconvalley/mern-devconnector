99b187dc557a3daea26068a418e7e8e36733acb4
John Dawes <jdawes@bluebitmedia.com>
2018-06-30 16:06:02 -0600

    REACT: Navigation and Footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+// Layout Components
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+
+// Boostrap App-Wide Style sheet
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+				<h1>React App Hello</h1>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
