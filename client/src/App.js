7fb943da95da1d7db3e5498bf2181c04f497744f
John Dawes <jdawes@bluebitmedia.com>
2018-06-30 16:43:31 -0600

    REACT: Router and Link for landing, login, registration pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 // Layout Components
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import Landing from "./components/Layout/Landing";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 
 // Boostrap App-Wide Style sheet
 import "./App.css";
@@ -11,11 +14,17 @@ import "./App.css";
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Navbar />
-				<Landing />
-				<Footer />
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+					<div className="container">
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/register" component={Register} />
+					</div>
+					<Footer />
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
