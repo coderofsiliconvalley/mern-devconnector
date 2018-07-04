80a11929295d2e22698ea1f15458f5111b336301
John Dawes <jdawes@bluebitmedia.com>
2018-07-04 13:53:24 -0600

    Added mapStateToProps for getting Profile content from Redux store

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions";
-//import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
 	componentDidMount() {
@@ -9,21 +9,34 @@ class Dashboard extends Component {
 	}
 
 	render() {
+		let userInfo = "";
+		if (this.props.profile.profile) {
+			userInfo = this.props.profile.profile.handle;
+		}
+
 		return (
 			<div>
-				<h1>Dashboard loaded</h1>
+				{userInfo && <h1>Dashboard loaded for:</h1>}
+				{userInfo}
 			</div>
 		);
 	}
 }
 
+Dashboard.propTypes = {
+	onGetUserProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired
+};
+
 const mapStateToProps = state => {
 	return {
 		auth: state.auth,
-		errors: state.errors,
+		//errors: state.errors,
 		profile: state.profile
 	};
 };
+
 const mapDispatchToProps = dispatch => {
 	return {
 		onGetUserProfile: () => dispatch(actions.getCurrentProfile())
