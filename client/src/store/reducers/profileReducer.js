f95d25ca36f501dba4019545f3f6abf666055efe
John Dawes <jdawes@bluebitmedia.com>
2018-07-04 13:07:00 -0600

    Profile Reducer/Actions placeholder

---
+++ client/src/store/reducers/profileReducer.js
@@ -0,0 +1,15 @@
+import * as actionTypes from "../actions/actionTypes";
+//import isEmpty from "../../validation/is-empty";
+
+const initialState = {
+	hello: ""
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default reducer;
