97a7bfbf59c047c4b3d72f31a62ed6c053c43a5d
John Dawes <jdawes@bluebitmedia.com>
2018-07-11 13:22:16 -0600

    Profile About component

--- client/src/components/Profile/ProfileAbout.js
+++ client/src/components/Profile/ProfileAbout.js
@@ -1,9 +1,34 @@
 import React from "react";
+import isEmpty from "../../validation/is-empty";
 
 const profileAbout = props => {
 	const { profile } = props;
 
-	return <div>About {profile.handle}</div>;
+	// Get first name
+	const firstName = profile.user.name.trim().split(" ")[0];
+
+	// Skill list
+	const skills = profile.skills.map((skill, index) => (
+		<div key={index} className="p-3">
+			<i className="fa fa-check" /> {skill}
+		</div>
+	));
+
+	return (
+		<div className="row">
+			<div className="col-md-12">
+				<div className="card card-body bg-light mb-3">
+					<h3 className="text-center text-info">{firstName}'s Bio</h3>
+					<p className="lead">{isEmpty(profile.bio) ? null : profile.bio}</p>
+					<hr />
+					<h3 className="text-center text-info">Skill Set</h3>
+					<div className="row">
+						<div className="d-flex flex-wrap justify-content-center align-items-center">{skills}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default profileAbout;
