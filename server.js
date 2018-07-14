a82bfebb45084f90d9bb2eecb9b34ccd819c7d5d
John Dawes <jdawes@bluebitmedia.com>
2018-07-14 08:35:13 -0600

    Readme Update

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === "production") {
 	// Set static folder
 	app.use(express.static("client/build"));
 	app.get("*", (req, res) => {
-		console.log(path.resolve(__dirname, "client", "build", "index.html"));
+		//console.log(path.resolve(__dirname, "client", "build", "index.html"));
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
