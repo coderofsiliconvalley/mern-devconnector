19213b3d748fb20281333c69a41c7765a1db64e5
John Dawes <jdawes@bluebitmedia.com>
2018-07-13 19:59:49 -0600

    Heroku build configuration

--- server.js
+++ server.js
@@ -71,5 +71,5 @@ if (process.env.NODE_ENV === "production") {
 //
 // START APP
 //
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server running on port: ", port));
